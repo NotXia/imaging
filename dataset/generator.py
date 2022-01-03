3c0c7bcb6954168ebc86a356bf4de7dafeed9695
Xia <tiancheng.xia2001@gmail.com>
2022-01-03 11:13:53 +0100

    Better generator

--- dataset/generator.py
+++ dataset/generator.py
@@ -2,38 +2,50 @@ from PIL import Image
 from PIL import ImageDraw
 import random
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-# https://stackoverflow.com/questions/23411688/drawing-polygon-with-n-number-of-sides-in-python-3-2
+# Restituisce la lista dei vertici di un poligono fissate alcuni valori
+# Fonte: https://stackoverflow.com/questions/23411688/drawing-polygon-with-n-number-of-sides-in-python-3-2
 def polygon(sides, radius=1, rotation=0, start_coord=(0, 0)):
     one_segment = math.pi * 2 / sides
     points = [(math.sin(one_segment * i + rotation) * radius, math.cos(one_segment * i + rotation) * radius) for i in range(sides)]
     points = [(point[0]+start_coord[0], point[1]+start_coord[1]) for point in points]
     return points
 
-def generate_figure(w, h):
-    out_image = Image.new("RGBA", (w, h))
-
-    num_polygons = random.randrange(3, 6)
+def generate_image(width, height, min_poygons=3, max_polygons=6, max_polygon_sides=10):
+    out_image = Image.new("RGBA", (width, height))
+    num_polygons = random.randrange(min_poygons, max_polygons)
+    
     for i in range(num_polygons):
-        # Immagine con il nuovo poligono
-        image = Image.new("RGBA", (w, h))
+        # Immagine che conterrà il nuovo poligono
+        image = Image.new("RGBA", (width, height))
         draw = ImageDraw.Draw(image)
 
+        # Generazione punti
         points = polygon(
-            sides = random.randrange(3, 10), 
-            radius = random.randrange(int(w/80), int(w/3)), 
+            sides = random.randrange(3, max_polygon_sides), 
+            radius = random.randrange(int(width/60), int(width/3)), 
             rotation = random.randrange(0, 359),
-            start_coord = (random.randrange(0, w), random.randrange(0, h))
+            start_coord = (random.randrange(0, width), random.randrange(0, height))
         )
 
+        # Selezione colore
         color = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255), 150)
         draw.polygon((points), fill=color)
 
         # Sovrapposizione delle immagini
         out_image = Image.alpha_composite(out_image, image)
 
-    return out_image.convert("L")
+    # return out_image.convert("L")
+    return np.array(out_image.convert("L")) / 255  # Immagine in scala di grigi normalizzata in [0, 1]
 
-image = generate_figure(512, 512)
-image.show()
-image.save("test.png")
+
+if __name__ == "__main__":
+    image = generate_image(512, 512)
+    # image.show()
+    # image.save("test.png")
+
+    print(np.min(image), np.max(image))
+    plt.imshow(image, cmap="gray")
+    plt.show()
